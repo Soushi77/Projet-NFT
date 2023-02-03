bd04bdf5d9624ffb133470813605bce5c22c813a
Raphael Misaiphon <misaiphonraphael@gmail.com>
2023-02-03 01:52:44 +0100

    test close popin

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
     //CA NE MARCHE PAAAAAS, HELP ME RAPHOU
 
-    let btn1 = document.getElementsByClassName("btn1");
-    let btn3 = document.getElementsByClassName("btn3");
-    let background_cookies_popin = document.getElementsByClassName("background_cookies_popin");
+let btn1 = document.getElementsByClassName("btn1");
+let btn3 = document.getElementsByClassName("btn3");
+let cookies_popin = document.getElementsByClassName("cookies_popin");
     
-    btn1.addEventListener("click", () => {
-      if(getComputedStyle(background_cookies_popin).display != "none"){
-        background_cookies_popin.style.display = "none";}
-    });
+// btn1.addEventListener("click", () => {
+//   if(getComputedStyle(background_cookies_popin).display != "none"){
+//   background_cookies_popin.style.display = "none";}
+// });
 
-    btn3.addEventListener("click", () => {
-        if(getComputedStyle(background_cookies_popin).display != "none"){
-          background_cookies_popin.style.display = "none";}
-      });
+// btn3.addEventListener("click", () => {
+//    if(getComputedStyle(background_cookies_popin).display != "none"){
+//   background_cookies_popin.style.display = "none";}
+// });
+
+// btn1.addEventListener("click", function() {
+//   cookies_popin.classList.add('hidden');
+// });
+
+
+const closeModal = function () {
+  cookies_popin.classList.add('hidden');
+};
+
+btn1.addEventListener('click', closeModal);
